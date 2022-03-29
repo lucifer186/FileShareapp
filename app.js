08fec59ec7b7fcfe63035de97507be3171e63700
Manoj <manojmahawar1123@gmail.com>
2022-03-29 21:11:41 +0530

    Intial Commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+const cors= require('cors')
 const fileRoute = require('./routes/file')
 const showRoute = require('./routes/show')
 const downloadRoute = require('./routes/download.js')
@@ -12,7 +13,10 @@ const PORT = process.env.PORT || 3000
 const connectDB = require('./config/db')
 
 connectDB()
-
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(','),
+  }
+app.use(cors(corsOptions))
 app.use(express.static('public'))
 app.use(express.json())
 app.use('/api/files', fileRoute)
